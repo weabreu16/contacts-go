7565addf498e6cc970544c1c0826b45772f254ae
William Abreu <weabreu16@gmail.com>
2023-01-29 21:51:08 -0400

    feat(contacts): rename contact route to contacts and update requires id

--- api/routes/contact.go
+++ api/routes/contact.go
@@ -13,11 +13,11 @@ type ContactRoutes struct {
 func (self ContactRoutes) Setup() {
 	api := self.handler.Gin.Group("/api")
 	{
-		api.GET("/contact/:id", self.contactController.GetContact)
-		api.GET("/contact/user/:userId", self.contactController.GetContacts)
-		api.POST("/contact", self.contactController.CreateContact)
-		api.PUT("/contact", self.contactController.UpdateContact)
-		api.DELETE("/contact/:id", self.contactController.DeleteContact)
+		api.GET("/contacts/:id", self.contactController.GetContact)
+		api.GET("/contacts/user/:userId", self.contactController.GetContacts)
+		api.POST("/contacts", self.contactController.CreateContact)
+		api.PUT("/contacts/:id", self.contactController.UpdateContact)
+		api.DELETE("/contacts/:id", self.contactController.DeleteContact)
 	}
 }
 

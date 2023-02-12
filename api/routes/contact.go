43eefdb324170f46102d5e11b632d67bfc7d2097
William Abreu <weabreu16@gmail.com>
2023-02-12 14:43:44 -0400

    feat: contacts endpoints require jwt

--- api/routes/contact.go
+++ api/routes/contact.go
@@ -2,16 +2,18 @@ package routes
 
 import (
 	"contacts-go/api/controllers"
+	"contacts-go/api/middlewares"
 	"contacts-go/lib"
 )
 
 type ContactRoutes struct {
 	handler           lib.RequestHandler
 	contactController controllers.ContactController
+	jwtMiddleware     middlewares.JWTMiddleware
 }
 
 func (self ContactRoutes) Setup() {
-	api := self.handler.Gin.Group("/api")
+	api := self.handler.Gin.Group("/api", self.jwtMiddleware.Handler())
 	{
 		api.GET("/contacts", self.contactController.GetContacts)
 		api.GET("/contacts/:id", self.contactController.GetContact)
@@ -20,15 +22,18 @@ func (self ContactRoutes) Setup() {
 		api.POST("/contacts/:id/image", self.contactController.UploadImage)
 		api.PUT("/contacts/:id", self.contactController.UpdateContact)
 		api.DELETE("/contacts/:id", self.contactController.DeleteContact)
+		api.DELETE("/contacts/image/:id", self.contactController.DeleteImage)
 	}
 }
 
 func NewContactRoutes(
 	handler lib.RequestHandler,
 	contactController controllers.ContactController,
+	jwtMiddleware middlewares.JWTMiddleware,
 ) ContactRoutes {
 	return ContactRoutes{
 		handler:           handler,
 		contactController: contactController,
+		jwtMiddleware:     jwtMiddleware,
 	}
 }
